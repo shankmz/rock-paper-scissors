a2dfb40527a1ef015c2b423799120e658dc08fe3
shankmz <shankarzamora@gmail.com>
2022-12-15 12:50:01 -0700

    browser displays the winner of each round. Adding code to declare winner after 5 rounds

--- rps.js
+++ rps.js
@@ -1,86 +1,64 @@
-//Make a list of 3 options- Rock, Paper, Scissor
-//Ask the player’s name so we can display it on the scoreboard
-//Initialize the scores of player and computer to 0 and number of rounds to 0.
-//Set the gameOn flag to True
-//While gameOn flag is True repeat steps 6 to 12
-//Randomly generate one of the options from the list as ComputerOption
-//Let the player choose any option from Rock, Paper, Scissor as PlayerOption.
-//Increase number of rounds by 1
-//If both player and computer, choose the same option then the current round ends in a draw. Else proceed to step 10
-//Determine the winner based on the rules and increase the score of the winner by 1.
-//Display the updated score.
-//If the number of rounds is greater than 5 then set the gameOn flag to false and end the game.
-//Choose and display the final winner based on the total score.
-
-
 const compChoice = ["rock", "paper", "scissors"];
-let computerScore = 0;
-let playerScore = 0;
 
-function game() {
-    for (let i = 0; i <= 4; i++) {
-    playRound();
-    
-    
-}   const gameWinner = winnerGame(playerScore, computerScore);
-    console.log(gameWinner);
+const selectionButtons = document.querySelectorAll("[data-selection]");
+const playerScoreBoard = document.querySelector('[data-player-score');
+const computerScoreBoard = document.querySelector('[data-computer-score');
+const tieScoreBoard = document.querySelector('[data-ties-score');
 
-}
-function playRound(){
-    const playerSelection = playerChoice();
+
+selectionButtons.forEach(selectionButtons => {
+    selectionButtons.addEventListener('click', e => {
+        const selectionName = selectionButtons.dataset.selection
+        makeSelection(selectionName)
+    })
+})
+
+function makeSelection(selection) {
     const computerSelection = getComputerChoice();
-    const winner = checkWinner(playerSelection, computerSelection);
-    console.log(winner);
+    const winner = checkWinner(selection, computerSelection)
+    console.log(winner)
 }
 
 function getComputerChoice() {
     return compChoice [Math.floor(Math.random() * compChoice.length)];
 }
 
-function playerChoice() {
-    let playerInput = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
-    return playerInput;
+function incrementScore(scoreBoard){
+    scoreBoard.innerText = parseInt(scoreBoard.innerText) + 1
 }
+
+let computerScore = document.querySelector("computer-score");
+let ties = document.querySelector("ties");
+let playerScore = document.querySelector("player-score");
+
 
 function checkWinner(choiceP, choiceC) {
 
     if (choiceP === choiceC) {
-        return "It's a tie";
+        return incrementScore(tieScoreBoard) + ++ties;
    }
    else if (choiceP === "rock" && choiceC === "paper") {
-       return "You Lose " + ++computerScore;
+       return incrementScore(computerScoreBoard) + ++computerScore;
    }
    
    else if (choiceP === "rock" && choiceC === "scissors") {
-       return "You Win! " + ++playerScore;
+       return incrementScore(playerScoreBoard) + ++playerScore;
    }
    
    else if (choiceP === "scissors" && choiceC === "rock") {
-       return "You Lose " + ++computerScore;
+       return incrementScore(computerScoreBoard) + ++computerScore;
    }
    
    else if (choiceP === "scissors" && choiceC === "paper") {
-       return "You Win! " + ++playerScore;
+       return ++playerScore;
    }
    
    else if (choiceP === "paper" && choiceC === "scissors") {
-       return "You Lose! " + ++computerScore;
+       return incrementScore(computerScoreBoard) + ++computerScore;
    }
    
    else (choiceP === "paper" && choiceC === "rock"); {
-       return "You Win! " + ++playerScore;
+       return incrementScore(playerScoreBoard) + ++playerScore;
    }
 }
-
-function winnerGame (winnerP, winnerC) {
-    if (winnerP === winnerC) {
-        return "Tie Game";
-    } else if (winnerP > winnerC) {
-        return "Game is Yours!";
-    } else {
-        return "Computers Game";
-    }
-}
-
-game();
 
